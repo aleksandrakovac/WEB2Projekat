9df311525cece779241ceea344bb75ac936ac53c
aleksandrakovac <39497346+aleksandrakovac@users.noreply.github.com>
2019-05-27 21:47:31 +0200

    project

---
+++ WebApp/Models/Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class Product
+    {
+        private int id;
+        private string name;
+        private string price;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+    }
+}
