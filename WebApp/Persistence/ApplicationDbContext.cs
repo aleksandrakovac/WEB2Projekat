a87a9cfed772f27ab3d19d9e865d6c6305aa0d64
dejanagrujic <39495830+dejanagrujic@users.noreply.github.com>
2019-06-05 12:41:58 +0200

    update dbcontext

--- WebApp/Persistence/ApplicationDbContext.cs
+++ WebApp/Persistence/ApplicationDbContext.cs
@@ -11,7 +11,20 @@ namespace WebApp.Persistence
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public DbSet<Product> products { get; set; }
+        public DbSet<Linija> Linija { get; set; }
+        public DbSet<DanUNedelji> DanUNedelji { get; set; }
+        public DbSet<Polazak> Polazak { get; set; }
+        public DbSet<Stanica> Stanica { get; set; }
+        public DbSet<TipPolaska> TipPolaska { get; set; }
+        public DbSet<Vozilo> Vozilo { get; set; }
+        public DbSet<Karta> Karta { get; set; }
+        public DbSet<TipKarte> TipKarte { get; set; }
+        public DbSet<TipKorisnika> TipKorisnika { get; set; }
+        public DbSet<CijenaKarte> CijenaKarte { get; set; }
+        public DbSet<Cjenovnik> Cjenovnik { get; set; }
+
+
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
