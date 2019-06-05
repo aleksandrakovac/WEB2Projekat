0a3af190d27188d4d33300e20d94461b92ce2976
dejanagrujic <39495830+dejanagrujic@users.noreply.github.com>
2019-06-05 13:28:14 +0200

    Update Repository

---
+++ WebApp/Persistence/Repository/RepositoryKarta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApp.Models;
+
+namespace WebApp.Persistence.Repository
+{
+    public class RepositoryKarta : Repository<Karta, int>, IRepositroyKarta
+    {
+        public RepositoryKarta(DbContext context) : base(context)
+        {
+
+        }
+
+    }
+}
