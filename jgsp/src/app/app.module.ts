c9eb4b28ac7359e978cfb1d879c5f57f2b024b61
aleksandrakovac <39497346+aleksandrakovac@users.noreply.github.com>
2019-06-06 12:45:13 +0200

    promene u angularu

--- jgsp/src/app/app.module.ts
+++ jgsp/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { LoginComponent } from './login/login.component';
+import { ModelsComponent } from './models/models.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    LoginComponent,
+    ModelsComponent
   ],
   imports: [
     BrowserModule,
--- jgsp/src/app/mock-heroes.ts
+++
@@ -1,8 +0,0 @@
-import { hero } from "./hero";
-
-export const HEROES: hero[] =
-[
-   { id: 11, name: 'Goku' },
-   { id: 12, name: 'Narco'}
- 
-]
