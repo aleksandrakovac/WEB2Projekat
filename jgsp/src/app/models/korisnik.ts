9b4b2632e1437f9968c4e78931de9d00178373c5
aleksandrakovac <39497346+aleksandrakovac@users.noreply.github.com>
2019-06-06 16:32:29 +0200

    korisnik.ts

---
+++ jgsp/src/app/models/korisnik.ts
@@ -0,0 +1,10 @@
+export class Korisnik{
+    KorisnickoIme: string; 
+    Lozinka: string;
+    PotvrdaLozinke: string;
+    Email: string;
+    Ime: string;
+    Prezime: string;
+    Adresa: string;
+    Datum: string;
+}
