dcb10c9641db3889e5dcf49ddc5336f66fe44ff1
dejanagrujic <39495830+dejanagrujic@users.noreply.github.com>
2019-06-06 16:41:24 +0200

    registracija update

--- jgsp/src/app/registracija/registracija.component.ts
+++ jgsp/src/app/registracija/registracija.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthHttpService } from 'src/services/http/auth.service';
+import { Router } from '@angular/router';
+import { NgForm, FormBuilder, Validators } from '@angular/forms';
+import { Korisnik } from '../models/korisnik';
 
 @Component({
   selector: 'app-registracija',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistracijaComponent implements OnInit {
 
-  constructor() { }
+  registracijaForm = this.fb.group({
+    Ime: ['', Validators.required],
+    Prezime: ['', Validators.required],
+    KorisnickoIme: ['', Validators.required],
+    Email: ['', Validators.required],
+    Lozinka: ['', Validators.required],
+    PotvrdaLozinke: ['', Validators.required],
+    Adresa: ['', Validators.required],
+    Datum: ['', Validators.required],
+  });
 
+  constructor(private http:AuthHttpService, private router: Router, private fb: FormBuilder) { }
+  
   ngOnInit() {
+  }
+
+  registracija()
+  {
+    let registracijaModel: Korisnk = this.registracijaForm.value;
+    this.http.register(registracijaModel).subscribe(temp => {
+      if(temp == "uspijesno")
+      {
+        console.log(temp);
+        this.router.navigate(["/pocetna"]);
+      }
+      else if(temp == "neuspijesno")
+      {
+        console.log(temp);
+        this.router.navigate(["/login"]);
+      }
+    })
   }
 
 }
