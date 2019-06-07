7b81b6697649bc43b2d4b53407fbd94d6f31e2a2
dejanagrujic <39495830+dejanagrujic@users.noreply.github.com>
2019-06-07 18:38:15 +0200

    Update TypeTicketsController.cs

--- web2/WebApp/WebApp/Controllers/TypeTicketsController.cs
+++ web2/WebApp/WebApp/Controllers/TypeTicketsController.cs
@@ -1,17 +1,124 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
+using System.Net;
 using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
 using System.Web.Mvc;
+using WebApp.Models;
+using WebApp.Persistence.UnitOfWork;
 
 namespace WebApp.Controllers
 {
-    public class TypeTicketsController : Controller
+    public class TypeTicketsController : ApiController
     {
-        // GET: TypeTickets
-        public ActionResult Index()
+        private IUnitOfWork unitOfWork;
+
+        public TypeTicketsController(IUnitOfWork unitOfWork)
         {
-            return View();
+            this.unitOfWork = unitOfWork;
+        }
+
+        // GET: api/TypeTickets
+        public IEnumerable<TypeTicket> GetTypeTicket()
+        {
+            return unitOfWork.TypeTicketRepository.GetAll();
+        }
+
+        // GET: api/TypeTickets/5
+        [ResponseType(typeof(TypeTicket))]
+        public IHttpActionResult GetTypeTicket(int id)
+        {
+            TypeTicket typeTicket = unitOfWork.TypeTicketRepository.Get(id);
+            if (typeTicket == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(typeTicket);
+        }
+
+        // PUT: api/TypeTickets/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTypeTicket(int id, TypeTicket typeTicket)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != typeTicket.Id)
+            {
+                return BadRequest();
+            }
+
+
+
+            try
+            {
+                unitOfWork.TypeTicketRepository.Update(typeTicket);
+                unitOfWork.Complete();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TypeTicketExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/TypeTickets
+        [ResponseType(typeof(TypeTicket))]
+        public IHttpActionResult PostTypeTicket(TypeTicket typeTicket)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            unitOfWork.TypeTicketRepository.Add(typeTicket);
+            unitOfWork.Complete();
+
+            return CreatedAtRoute("DefaultApi", new { id = typeTicket.Id }, typeTicket);
+        }
+
+        // DELETE: api/TypeTickets/5
+        [ResponseType(typeof(TypeTicket))]
+        public IHttpActionResult DeleteTypeTicket(int id)
+        {
+            TypeTicket typeTicket = unitOfWork.TypeTicketRepository.Get(id);
+            if (typeTicket == null)
+            {
+                return NotFound();
+            }
+
+            unitOfWork.TypeTicketRepository.Remove(typeTicket);
+            unitOfWork.Complete();
+
+            return Ok(typeTicket);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                unitOfWork.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TypeTicketExists(int id)
+        {
+            return unitOfWork.TypeTicketRepository.Find(e => e.Id == id).Count() > 0;
         }
     }
 }
