b6c26b4a13e4483ce223079afb1c8e5f0feade30
aleksandrakovac <39497346+aleksandrakovac@users.noreply.github.com>
2019-06-07 14:56:24 +0200

    model

--- web2/WebApp/WebApp/Models/Pricelist.cs
+++ web2/WebApp/WebApp/Models/Pricelist.cs
@@ -7,5 +7,10 @@ namespace WebApp.Models
 {
     public class Pricelist
     {
+        public int Id { get; set; }
+        public int StationId { get; set; }
+        public int Number { get; set; }
+        public List<Station> Stations { get; set; }
+        public List<Timetable> Timetables { get; set; }
     }
 }
