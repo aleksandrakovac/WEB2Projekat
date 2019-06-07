eaaf9bd1ab1b4ff9fdcd21ddb4c1a89c07f5331f
dejanagrujic <39495830+dejanagrujic@users.noreply.github.com>
2019-06-07 18:32:06 +0200

    Ticket of price controller

---
+++ web2/WebApp/WebApp/Controllers/TicketsOfPriceController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApp.Models;
+using WebApp.Persistence;
+using WebApp.Persistence.UnitOfWork;
+
+namespace WebApp.Controllers
+{
+    [RoutePrefix("api/TicketOfPrice")]
+    public class TicketsOfPriceController : ApiController
+    {
+        private IUnitOfWork unitOfWork;
+
+        public TicketsOfPriceController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        [AllowAnonymous]
+        [ResponseType(typeof(float))]
+        [Route("GetKarta/{tip}")]
+        public IHttpActionResult GetKartaCena(string tip)
+        {
+            List<TypeTicket> karte = unitOfWork.TypeTicketRepository.GetAll().ToList();
+            TicketPrice price = new TicketPrice();
+            float cena = 0;
+            foreach (TypeTicket k in karte)
+            {
+                if (tip == k.Type)
+                {
+                    price = unitOfWork.PriceOfTicketRepository.Get(k.Id);
+                    cena = price.Price;
+                }
+            }
+
+            if (karte == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cena);
+        }
+
+        // GET: api/PriceOfTickets
+        public IEnumerable<TicketPrice> GetPriceOfTicket()
+        {
+            return unitOfWork.PriceOfTicketRepository.GetAll();
+        }
+
+        // GET: api/PriceOfTickets/5
+        [ResponseType(typeof(TicketPrice))]
+        public IHttpActionResult GetPriceOfTicket(int id)
+        {
+            TicketPrice priceOfTicket = unitOfWork.PriceOfTicketRepository.Get(id);
+            if (priceOfTicket == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(priceOfTicket);
+        }
+
+        // PUT: api/PriceOfTickets/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPriceOfTicket(int id, PriceOfTicket priceOfTicket)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != priceOfTicket.Id)
+            {
+                return BadRequest();
+            }
+
+
+
+            try
+            {
+                unitOfWork.PriceOfTicketRepository.Update(priceOfTicket);
+                unitOfWork.Complete();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PriceOfTicketExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/PriceOfTickets
+        [ResponseType(typeof(TicketPrice))]
+        public IHttpActionResult PostPriceOfTicket(TicketPrice priceOfTicket)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            unitOfWork.PriceOfTicketRepository.Add(priceOfTicket);
+            unitOfWork.Complete();
+
+            return CreatedAtRoute("DefaultApi", new { id = priceOfTicket.Id }, priceOfTicket);
+        }
+
+        // DELETE: api/PriceOfTickets/5
+        [ResponseType(typeof(TicketPrice))]
+        public IHttpActionResult DeletePriceOfTicket(int id)
+        {
+            TicketPrice priceOfTicket = unitOfWork.PriceOfTicketRepository.Get(id);
+            if (priceOfTicket == null)
+            {
+                return NotFound();
+            }
+
+            unitOfWork.PriceOfTicketRepository.Remove(priceOfTicket);
+            unitOfWork.Complete();
+
+            return Ok(priceOfTicket);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                unitOfWork.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PriceOfTicketExists(int id)
+        {
+            return unitOfWork.PriceOfTicketRepository.Find(e => e.Id == id).Count() > 0;
+        }
+    }
+}
