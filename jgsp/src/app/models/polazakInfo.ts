42b75f85e4ced74b5cfe17a0fd76ab04079b2801
dejanagrujic <39495830+dejanagrujic@users.noreply.github.com>
2019-06-07 14:38:54 +0200

    Delete polazakInfo.ts

--- jgsp/src/app/models/polazakInfo.ts
+++
@@ -1,9 +0,0 @@
-import { TipPolaska } from './tipPolaska';
-import { DanUNedelji } from './danUNedelji';
-import { Linija } from './linija';
-
-export class PolazakInfo{
-    TipPolaska: TipPolaska[];
-    DanUNedelji: DanUNedelji[];
-    Linija: Linija[];
-}
